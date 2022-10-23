464724a8a36f3353e1bf4fd4f8ef6a7c4fbfcf18
Michal <michalstolarski@gmail.com>
2022-10-23 14:15:12 +0200

    feat: define GlobalStyle

--- src/@styles/GlobalStyle/GlobalStyle.tsx
+++ src/@styles/GlobalStyle/GlobalStyle.tsx
@@ -3,14 +3,32 @@ import { createGlobalStyle } from 'styled-components';
 import { defaultFontSize } from '@configs/const';
 
 const GlobalStyle = createGlobalStyle`
+    html,
+    body {
+        margin: 0;
+    }
+
     html {
         font-size: ${defaultFontSize}px;
     }
 
     body {
         font-family: ${props => props.theme.fonts.family.poppins};
-        font-size: 1rem;
-        font-weight: ${props => props.theme.fonts.weight.light};
+        font-size: ${props => props.theme.fonts.size.body};
+        font-weight: ${props => props.theme.fonts.weight.regular};
+        line-height: ${props => props.theme.fonts.lineHeight.body};
+        color: ${props => props.theme.pallete.black};
+        background-color: rgb(${props => props.theme.pallete.white});
+    }
+
+    * {
+        box-sizing: border-box;
+        
+        &:focus,
+        &:active,
+        &:visited {
+            outline: none;
+        }
     }
 `;
 
